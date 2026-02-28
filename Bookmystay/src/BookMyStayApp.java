f880a275c1ceb158cbb80a2995c66202879b64ac
BL-Anbu <anbarasu.annadurai@bridgelabz.com>
2026-03-01 00:36:49 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ Bookmystay/src/BookMyStayApp.java
@@ -0,0 +1,12 @@
+
+/**
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * @author Anbu A
+ */
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully...");
+    }
+}
